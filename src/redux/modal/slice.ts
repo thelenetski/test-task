cfde90711c0a56d9765cf3a5d754774978961155
thelenetski <49438367+thelenetski@users.noreply.github.com>
2025-03-11 20:25:43 +0200

    fix react modal

--- src/redux/modal/slice.ts
+++ src/redux/modal/slice.ts
@@ -1,7 +1,7 @@
 import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 
 interface ModalState {
-  isOpen?: boolean;
+  isOpen: boolean;
   type?: string | null;
   content?: unknown | null;
 }

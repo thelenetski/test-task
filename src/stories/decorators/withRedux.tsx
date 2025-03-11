f0160e928bbbf4beb046f75b0a3f9cc9ce0dea59
thelenetski <49438367+thelenetski@users.noreply.github.com>
2025-03-11 20:29:57 +0200

    fix story type

--- src/stories/decorators/withRedux.tsx
+++ src/stories/decorators/withRedux.tsx
@@ -1,7 +1,8 @@
 import { Provider } from "react-redux";
 import { store } from "../../redux/store"; // Импортируй свой store
+import { Decorator } from "@storybook/react/*";
 
-const withRedux = (Story) => (
+const withRedux: Decorator = (Story) => (
   <Provider store={store}>
     <Story />
   </Provider>
